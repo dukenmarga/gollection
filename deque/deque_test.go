33716a5c1f36bf977f2301e15ea04f30dbff8b5d
Duken Marga <dukenmarga@gmail.com>
2025-01-04 19:18:18 +0900

    deque: test PushRight function

--- deque/deque_test.go
+++ deque/deque_test.go
@@ -53,3 +53,49 @@ func TestNewDequeueFromString(t *testing.T) {
 		})
 	}
 }
+
+func TestPushRight(t *testing.T) {
+	tests := []testCase[string]{
+		{
+			name: "Test push right using string values",
+			input: []string{
+				"10",
+				"20",
+			},
+			wantDequeVal: []string{
+				"10",
+				"20",
+			},
+		},
+		{
+			name: "Test push right from several empty string",
+			input: []string{
+				"",
+				"",
+			},
+			wantDequeVal: []string{
+				"",
+				"",
+			},
+		},
+		{
+			name:         "Test no push right from empty list",
+			input:        []string{},
+			wantDequeVal: []string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := NewDequeue[string]([]string{})
+			for _, input := range tt.input {
+				list.PushRight(input)
+			}
+			for _, wantVal := range tt.wantDequeVal {
+				pop := list.PopLeft()
+				if wantVal != pop {
+					t.Errorf("NewDequeueList() = %v, want %v", pop, wantVal)
+				}
+			}
+		})
+	}
+}
