5d59ca1316ef98dfeb081f78ab6a26fd8d5f269d
Duken Marga <dukenmarga@gmail.com>
2025-01-15 06:24:48 +0900

    tree: add test to create binary search tree root

--- tree/binary_search_tree_test.go
+++ tree/binary_search_tree_test.go
@@ -102,3 +102,95 @@ func TestNewBinarySearchTreeFromArray(t *testing.T) {
 		})
 	}
 }
+
+func TestNewBinarySearchTreeRoot(t *testing.T) {
+	tests := []testTree[int, int]{
+		{
+			name: "Test new tree int values (1 node)",
+			inputKeys: []int{
+				10,
+			},
+			inputVals: []int{
+				10,
+			},
+			wantTreeVal: []int{
+				10,
+			},
+		},
+		{
+			name: "Test new tree int values (2 nodes incremental)",
+			inputKeys: []int{
+				10,
+				20,
+			},
+			inputVals: []int{
+				10,
+				20,
+			},
+			wantTreeVal: []int{
+				10,
+				20,
+			},
+		},
+		{
+			name: "Test new tree int values (2 nodes decremental)",
+			inputKeys: []int{
+				20,
+				10,
+			},
+			inputVals: []int{
+				20,
+				10,
+			},
+			wantTreeVal: []int{
+				10,
+				20,
+			},
+		},
+		{
+			name: "Test new tree from several zero int",
+			inputKeys: []int{
+				0,
+				0,
+			},
+			inputVals: []int{
+				0,
+				0,
+			},
+			wantTreeVal: []int{
+				0,
+				0,
+			},
+		},
+		{
+			name: "Test new tree int values many nodes",
+			inputKeys: []int{
+				5, 6, 2, 10, 12, 3, 1, 9,
+			},
+			inputVals: []int{
+				5, 6, 2, 10, 12, 3, 1, 9,
+			},
+			wantTreeVal: []int{
+				1, 2, 3, 5, 6, 9, 10, 12,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Add root
+			root := NewBSTRoot[int](tt.inputKeys[0], tt.inputVals[0])
+
+			// Add the remaining nodes
+			for i := 1; i < len(tt.inputKeys); i++ {
+				root.Add(tt.inputKeys[i], tt.inputVals[i])
+			}
+
+			got := root.InorderTraversal()
+			for i, wantVal := range tt.wantTreeVal {
+				if wantVal != got[i].value {
+					t.Errorf("actual = %v, want %v", got[i], wantVal)
+				}
+			}
+		})
+	}
+}
